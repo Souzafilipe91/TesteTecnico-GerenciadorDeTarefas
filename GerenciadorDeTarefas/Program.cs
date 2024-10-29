fd9f5099b981f5c0f47fe706742a0d42a2a5dbf7
Filipe Pimenta de Souza <fipisouza91@gmail.com>
2024-10-29 19:52:00 -0300

    correção do backend

--- GerenciadorDeTarefas/Program.cs
+++ GerenciadorDeTarefas/Program.cs
@@ -1,10 +1,17 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+using GerenciadorDeTarefas;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.FileProviders;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
 using System.IO;
 
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        new MySqlServerVersion(new Version(8, 0, 23))
+    ));
 
 
 builder.Services.AddControllers();
@@ -12,16 +19,21 @@ builder.Services.AddControllers();
 
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowAll", policy =>
-    {
-        policy.AllowAnyOrigin()
-              .AllowAnyMethod()
-              .AllowAnyHeader();
-    });
+    options.AddPolicy("AllowAll", builder =>
+        builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 });
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseDefaultFiles(new DefaultFilesOptions
 {
@@ -41,13 +53,10 @@ app.MapFallbackToFile("index.html", new StaticFileOptions
         Path.Combine(Directory.GetCurrentDirectory(), "ClientApp", "dist", "client-app"))
 });
 
-app.UseRouting();
 
-
-app.UseCors("AllowAll");      
+app.UseHttpsRedirection();
+app.UseCors("AllowAll");
 app.UseAuthorization();
-
-
 app.MapControllers();
 
 app.Run();
